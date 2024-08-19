abaf307579efb25e472aceb189dd1a86e43c5337
Neyna Nayak <140084295+neyhere07@users.noreply.github.com>
2024-08-19 20:54:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-//complete this code
-class Rectangle {}
 
-class Square extends Animal {}
+class Rectangle {
+	constructor(width, height){
+		this.width = width;
+		this.height = height;
+	}
+	
+	getArea(){
+		return (this.width * this.height);
+	}
+}
+
+class Square extends Rectangle {
+	constructor(height){
+	  super(height)
+		this.height = height;
+	}
+	  getPerimeter(){
+		return (4 * this.height);
+	}
+	
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
